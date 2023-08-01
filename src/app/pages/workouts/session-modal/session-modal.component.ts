6c0e3fa8ed542823bf358b1142625ef27288e98d
diegogglez <diegomiller7@gmail.com>
2023-08-01 12:03:04 +0200

    feat: basic validation

--- src/app/pages/workouts/session-modal/session-modal.component.ts
+++ src/app/pages/workouts/session-modal/session-modal.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { Workout } from 'src/app/models/workouts';
 import { TimerComponent } from './timer/timer.component';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { v4 as uuidv4 } from 'uuid';
 
 @Component({
@@ -45,18 +45,19 @@ export class SessionModalComponent  implements OnInit {
     for (let item of this.workout.exercises!) {
       const exercise = new FormGroup({
         sets: new FormArray([])
-      })
+      });
       
       for (let i = 0; i <= this.createSetsRange(item.sets).length; i++) {
-        (<FormArray>exercise.get('sets')).push(new FormGroup({
-          weight: new FormControl(null),
+         (<FormArray>exercise.get('sets')).push(new FormGroup({
+          weight: new FormControl(null, Validators.required),
           reps: new FormControl(item.reps),
-          rir: new FormControl(item.rir)
-        }))
-      }
+          rir: new FormControl(item.rir),
+          done: new FormControl(false, Validators.requiredTrue)
+        }));
+      };
       
-      sessionExercises.push(exercise)
-    }
+      sessionExercises.push(exercise);
+    };
 
     this.sessionForm = new FormGroup({
       id: new FormControl(uuidv4()),
