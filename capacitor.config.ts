11ad74d22a0a8214fa66efaa5558c6b9dfa27985
diegogglez <diegomiller7@gmail.com>
2023-09-26 13:05:59 +0200

    refactor: appId update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.diegogglez.powerlog',
   appName: 'power-log',
   webDir: 'www',
   server: {
