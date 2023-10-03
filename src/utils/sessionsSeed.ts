6b2bfd82f03e1fa37ec570b1c43dc863ddca068a
diegogglez <diegomiller7@gmail.com>
2023-10-03 13:19:58 +0200

    refactor: sessions seed update

--- src/utils/sessionsSeed.ts
+++ src/utils/sessionsSeed.ts
@@ -3,12 +3,13 @@ import { Session } from "src/app/models/workouts";
 export const sessionsSeed: Session[] = [
   {
     id: '1',
-    date: '18/9/2023',
+    date: '02/10/2023',
     workoutName: 'Torso 1',
     sessionTime: '01:36:48',
     exercises: [
       {
         name: 'weighted pull ups',
+        enterRpe: false,
         sets: [
           {
             weight: 20,
@@ -37,7 +38,42 @@ export const sessionsSeed: Session[] = [
         ]
       },
       {
+        name: 'Handstand push ups',
+        enterRpe: true,
+        sets: [
+          {
+            weight: 0,
+            reps: 5,
+            rir: null,
+            rpe: 8,
+            done: true
+          },
+          {
+            weight: 0,
+            reps: 3,
+            rir: null,
+            rpe: 9,
+            done: true
+          },
+          {
+            weight: 0,
+            reps: 2,
+            rir: null,
+            rpe: 9,
+            done: true
+          },
+          {
+            weight: 0,
+            reps: 2,
+            rir: null,
+            rpe: 9,
+            done: true
+          },
+        ]
+      },
+      {
         name: 'Australian Pull ups',
+        enterRpe: false,
         sets: [
           {
             weight: 30,
@@ -61,6 +97,7 @@ export const sessionsSeed: Session[] = [
       },
       {
         name: 'barbell row',
+        enterRpe: false,
         sets: [
           {
             weight: 25,
@@ -92,41 +129,47 @@ export const sessionsSeed: Session[] = [
   },
   {
     id: '2',
-    date: '20/9/2023',
+    date: '05/10/2023',
     workoutName: 'Pierna 1',
     sessionTime: '01:50:32',
     exercises: [
       {
         name: 'High bar squats',
+        enterRpe: true,
         sets: [
           {
             weight: 90,
             reps: 5,
-            rir: 2,
+            rir: null,
+            rpe: 7,
             done: true
           },
           {
             weight: 90,
             reps: 5,
-            rir: 2,
+            rir: null,
+            rpe: 7.5,
             done: true
           },
           {
             weight: 90,
             reps: 5,
-            rir: 2,
+            rir: null,
+            rpe: 8,
             done: true
           },
           {
             weight: null,
             reps: 8,
-            rir: 2,
+            rir: null,
+            rpe: 8.5,
             done: false
           },
         ]
       },
       {
         name: 'Leg extensions',
+        enterRpe: false,
         sets: [
           {
             weight: 50,
@@ -150,6 +193,7 @@ export const sessionsSeed: Session[] = [
       },
       {
         name: 'Bulgarian squats',
+        enterRpe: false,
         sets: [
           {
             weight: 15,
@@ -173,18 +217,19 @@ export const sessionsSeed: Session[] = [
       },
       {
         name: 'Hip trust',
+        enterRpe: false,
         sets: [
           {
             weight: null,
             reps: 10,
             rir: 2,
-            done: false
+            done: true
           },
           {
             weight: null,
             reps: 10,
             rir: 2,
-            done: false
+            done: true
           },
           {
             weight: null,
@@ -200,6 +245,254 @@ export const sessionsSeed: Session[] = [
           },
         ]
       },
+
+    ]
+  },
+  {
+    id: '3',
+    date: '09/10/2023',
+    workoutName: 'Torso 1',
+    sessionTime: '01:36:48',
+    exercises: [
+      {
+        name: 'weighted pull ups',
+        enterRpe: false,
+        sets: [
+          {
+            weight: 20,
+            reps: 8,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: 20,
+            reps: 8,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: null,
+            reps: 8,
+            rir: 2,
+            done: false
+          },
+          {
+            weight: null,
+            reps: 8,
+            rir: 2,
+            done: false
+          },
+        ]
+      },
+      {
+        name: 'Handstand push ups',
+        enterRpe: true,
+        sets: [
+          {
+            weight: 0,
+            reps: 5,
+            rir: null,
+            rpe: 8,
+            done: true
+          },
+          {
+            weight: 0,
+            reps: 3,
+            rir: null,
+            rpe: 9,
+            done: true
+          },
+          {
+            weight: 0,
+            reps: 2,
+            rir: null,
+            rpe: 9,
+            done: true
+          },
+          {
+            weight: 0,
+            reps: 2,
+            rir: null,
+            rpe: 9,
+            done: true
+          },
+        ]
+      },
+      {
+        name: 'Australian Pull ups',
+        enterRpe: false,
+        sets: [
+          {
+            weight: 30,
+            reps: 12,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: 25,
+            reps: 12,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: 25,
+            reps: 12,
+            rir: 2,
+            done: true
+          },
+        ]
+      },
+      {
+        name: 'barbell row',
+        enterRpe: false,
+        sets: [
+          {
+            weight: 25,
+            reps: 12,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: 25,
+            reps: 12,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: null,
+            reps: 12,
+            rir: 2,
+            done: false
+          },
+          {
+            weight: null,
+            reps: 12,
+            rir: 2,
+            done: false
+          },
+        ]
+      },
+    ]
+  },
+  {
+    id: '4',
+    date: '12/10/2023',
+    workoutName: 'Pierna 1',
+    sessionTime: '01:50:32',
+    exercises: [
+      {
+        name: 'High bar squats',
+        enterRpe: true,
+        sets: [
+          {
+            weight: 90,
+            reps: 5,
+            rir: null,
+            rpe: 7,
+            done: true
+          },
+          {
+            weight: 90,
+            reps: 5,
+            rir: null,
+            rpe: 7.5,
+            done: true
+          },
+          {
+            weight: 90,
+            reps: 5,
+            rir: null,
+            rpe: 8,
+            done: true
+          },
+          {
+            weight: null,
+            reps: 8,
+            rir: null,
+            rpe: 8.5,
+            done: false
+          },
+        ]
+      },
+      {
+        name: 'Leg extensions',
+        enterRpe: false,
+        sets: [
+          {
+            weight: 50,
+            reps: 12,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: 45,
+            reps: 12,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: 40,
+            reps: 12,
+            rir: 2,
+            done: true
+          }
+        ]
+      },
+      {
+        name: 'Bulgarian squats',
+        enterRpe: false,
+        sets: [
+          {
+            weight: 15,
+            reps: 10,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: 15,
+            reps: 10,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: 15,
+            reps: 10,
+            rir: 2,
+            done: true
+          },
+        ]
+      },
+      {
+        name: 'Hip trust',
+        enterRpe: false,
+        sets: [
+          {
+            weight: null,
+            reps: 10,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: null,
+            reps: 10,
+            rir: 2,
+            done: true
+          },
+          {
+            weight: null,
+            reps: 10,
+            rir: 2,
+            done: false
+          },
+          {
+            weight: null,
+            reps: 10,
+            rir: 2,
+            done: false
+          },
+        ]
+      },
+
     ]
   },
 
